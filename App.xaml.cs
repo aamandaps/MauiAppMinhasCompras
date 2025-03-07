60980e79395b25fca24f9660e7f9622d3f82e305
aamandaps <aamandaps01@gmail.com>
2025-03-07 16:12:38 -0300

    Conexão com o SQLite e configuração do front

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,31 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        //db campo
+        static SQLiteDatabaseHelper _db;
+
+        //db propriedade
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine
+                       (
+                         Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                         "banco_sqlite_compras.db3"
+                       );
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
@@ -9,6 +33,16 @@
             //Troca da tela inicial
            // MainPage = new AppShell();
            MainPage = new NavigationPage(new Views.ListaProduto());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
         }
     }
 }
--- Helpers/SQLiteDatabaseHelper.cs
+++ Helpers/SQLiteDatabaseHelper.cs
@@ -21,10 +21,15 @@ namespace MauiAppMinhasCompras.Helpers
         }
         public Task<List<Produto>> Update (Produto p)
         {
-            string sql = "UPDATE Produto SET Descricao =? , Quantidade =? , Preco=? WHERE Id=? ";
+            string sql = "UPDATE Produto SET Descricao =? ," +
+                " Quantidade =? ," +
+                " Preco=? WHERE Id=? ";
             return _conn.QueryAsync<Produto>
-                (sql , p.Descricao , p.Quantidade , p.Preco , p.Id );
-        
+                (sql , p.Descricao,
+                       p.Quantidade, 
+                       p.Preco , 
+                       p.Id
+                );
         }
         public Task<int> Delete (int id) 
         {
